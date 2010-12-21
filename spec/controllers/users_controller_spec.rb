d254cd5f76ae6690fd298e00e31ecb2f71a86403
focon <focon@xs4all.nl>
2010-12-21 19:30:56 +0100

    Added a sign-up form

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe UsersController do
 render_views
+
   it "should have the correct title" do
       get :new
       response.should have_selector('title', 
@@ -59,5 +60,25 @@ end
                                    :content => "Sign up")
     end
 
-
+describe "POST 'create'" do
+describe "failure" do
+    before(:each) do
+    @attr = { :name => "", :email => "", :password => "", :password_confirmation => "" }
+    end
+it "should not create a user" do
+lambda do
+post :create, :user => @attr
+end.should_not change(User, :count)
 end
+it "should have the right title" do
+post :create, :user => @attr
+response.should have_selector("title", :content => "Sign up")
+end
+it "should render the 'new' page" do
+post :create, :user => @attr
+response.should render_template('new')
+end
+end
+end
+end
+
