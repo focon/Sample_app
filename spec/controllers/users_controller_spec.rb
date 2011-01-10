b4d14dd46e5a99d7f2b7343d42009846e0ae5764
focon <focon@xs4all.nl>
2011-01-10 20:31:39 +0100

    user index with pagination

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -19,6 +19,11 @@ describe "for signed-in users" do
       @user=test_sign_in(Factory(:user))
       Factory(:user, :email => "another@example.org")
       Factory(:user, :email => "another@example.net")
+
+      30.times do
+      Factory(:user, :email=>Factory.next(:email))
+      end
+
       end
        it "should be successful" do
       get :index
@@ -31,10 +36,18 @@ describe "for signed-in users" do
 
      it "should have an element for each user" do
       get :index
-       User.all.each do |user|
+       User.paginate(:page=>1).each do |user|
       response.should have_selector("li", :content => user.name)
        end
        end
+      it "should paginate users" do
+         get :index
+        response.should have_selector('div.pagination')
+        response.should have_selector('span.disabled', :content =>"Previous")
+response.should have_selector('a', :href =>"/users?page=2", :content =>"2")
+response.should have_selector("a", :href =>"/users?page=2", :content =>"Next")
+
+      end
       end
 end
 
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,10 @@
 Factory.define :user do |user|
-
-
         user.name                      "focon"
         user.email                     "f@o.nl"
         user.password                  "foobar"
         user.password_confirmation     "foobar"
+end
+
+Factory.sequence :email do |n|
+"person-#{n}@example.com"
 end
